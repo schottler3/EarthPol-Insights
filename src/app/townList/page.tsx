79cfe653c789a54023e93633e9c2f3d5cfedae62
schottler3 <LucasSchottler@gmail.com>
2025-07-19 00:39:06 -0500

    nationList & townList pages search

--- src/app/townList/page.tsx
+++ src/app/townList/page.tsx
@@ -6,7 +6,9 @@ import TownItem from "./TownItem";
 export default function page() {
 
     const [towns, setTowns] = useState<TownItem[] | null>(null);
+    const [renderedTowns, setRenderedTowns] = useState<TownItem[] | null>(null);
     const [isLoadingTowns, setIsLoadingTowns] = useState(true);
+    const [query, setQuery] = useState<string>("");
 
     interface TownItem {
         index: number;
@@ -26,6 +28,7 @@ export default function page() {
                 const result = await response.json();
                 result.sort((a:TownItem, b:TownItem) => a.name.localeCompare(b.name));
                 setTowns(result);
+                setRenderedTowns(result);
                 setIsLoadingTowns(false);
             } catch (e) {
                 console.log("Failed to get town data in townList -- /api/towns") // Updated error message
@@ -38,14 +41,28 @@ export default function page() {
         fetchData();
     },[])
 
+    useEffect(() => {
+        if(query.length > 0){
+            setRenderedTowns(towns?.filter((town) => {
+                return town.name.includes(query)
+            }) || null)
+        } else {
+            setRenderedTowns(towns)
+        }
+    }, [query])
+
     return (
-        <div className="m-4 w-full h-full flex flex-col gap-2">
-            {
-                towns && towns.length >= 1 && !isLoadingTowns ?
-                <>
-                    <h1 className="text-center text-aqua1 text-4xl font-bold w-full">Towns: {towns?.length}</h1>
+    <div className="m-4 w-full h-full flex flex-col gap-2">
+        {
+            !isLoadingTowns ?
+            <>
+                <h1 className="text-white font-bold text-2xl">{`Search Towns(${towns?.length})`}</h1>
+                <div className="bg-gray1 drop-shadow-xl my-4 p-4 gap-8 items-center">
+                    <input className="w-1/4 h-[4vh] rounded-md p-1" placeholder="Search towns" type="text" onChange={(e) => setQuery(e.target.value)}></input>
+                </div>
+                {renderedTowns && renderedTowns.length > 0 ? ( // Show results if any
                     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-                        {towns.map((item: TownItem, index: number) => (
+                        {renderedTowns.map((item: TownItem, index: number) => (
                             <TownItem
                                 key={index}
                                 name={item.name}
@@ -53,12 +70,17 @@ export default function page() {
                             />
                         ))}
                     </div>
-                </>
-                :
-                <div className="flex items-center justify-center h-full w-full *:w-1/2">
-                    <LoadingLocation />
-                </div>
-            }
-        </div>
-    )
+                ) : (
+                    <div className="text-center text-aqua1 text-xl">
+                        No towns found matching "{query}"
+                    </div>
+                )}
+            </>
+            :
+            <div className="flex items-center justify-center h-full w-full *:w-1/2">
+                <LoadingLocation />
+            </div>
+        }
+    </div>
+)
 }
